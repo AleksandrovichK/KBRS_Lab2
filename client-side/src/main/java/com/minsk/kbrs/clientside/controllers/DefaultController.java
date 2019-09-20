1ff22112fef1901473395b14ccfa375d935ee7dc
AleksandrovichK <killertom1997@gmail.com>
2019-09-20 04:10:30 +0300

    Comments are fixed

--- client-side/src/main/java/com/minsk/kbrs/clientside/controllers/DefaultController.java
+++ client-side/src/main/java/com/minsk/kbrs/clientside/controllers/DefaultController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author KotsyubaT
+ * @author AleksandrovichK
  */
 @RestController
 public class DefaultController {
